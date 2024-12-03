d4551863492b5ff9b7a5ede1af8fa824827e79a7
yuhyun <dlugus@naver.com>
2024-12-03 20:55:47 +0900

    이름 변경

--- src/BOJ/Main_2884_알람시계.java
+++ src/BOJ/Main_2884_알람시계.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Boj_2884 {
+public class Main_2884_알람시계 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
