ee3efd4c1dcbdae0c9b1b869d4134daef9e01dec
yuhyun <dlugus@naver.com>
2024-12-04 20:10:46 +0900

    15552 clear

--- src/BOJ/Main_15552_빠른AB합.java
+++ src/BOJ/Main_15552_빠른AB합.java
@@ -5,17 +5,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_10950 {
+public class Main_15552_빠른AB합 {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 1; i < num + 1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
-            System.out.println(a + b);
+            sb.append(a + b).append("\n");
         }
+        br.close();
+        System.out.println(sb);
     }
 }
+
