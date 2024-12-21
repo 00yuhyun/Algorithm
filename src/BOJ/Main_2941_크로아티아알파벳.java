e7d9983c1a2898c506b047671897d649ddfbdcb9
yuhyun <dlugus@naver.com>
2024-12-22 02:40:09 +0900

    2941 clear

--- src/BOJ/Main_2941_크로아티아알파벳.java
+++ src/BOJ/Main_2941_크로아티아알파벳.java
@@ -6,6 +6,22 @@ import java.io.InputStreamReader;
 
 public class Main_2941_크로아티아알파벳 {
     public static void main(String[] args) throws IOException {
-        // TODO 다시풀기
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int cnt = 0;
+        String[] croatia = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+        for (String s : croatia) {
+            while (input.contains(s)) {
+                cnt += 1;
+                input = input.replaceFirst(s, " ");
+            }
+        }
+
+        input = input.replace(" ", "");
+        cnt += input.length();
+
+        System.out.println(cnt);
     }
 }
