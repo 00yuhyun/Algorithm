5ebe79639c8b8d93468df8f3f098caa504c4a348
yuhyun <dlugus@naver.com>
2024-12-25 00:51:56 +0900

    1157 clear

--- src/BOJ/Main_1157_단어공부.java
+++ src/BOJ/Main_1157_단어공부.java
@@ -6,6 +6,27 @@ import java.io.InputStreamReader;
 
 public class Main_1157_단어공부 {
     public static void main(String[] args) throws IOException {
-        // TODO 다시풀기
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine().toUpperCase();
+        int[] intArrays = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            intArrays[s.charAt(i) - 'A'] += 1;
+        }
+
+        int max = 0;
+        char answer = '?';
+        for (int i = 0; i < 26; i++) {
+            if (intArrays[i] > max) {
+                max = intArrays[i];
+                answer = (char) (i + 'A');
+            } else if (max == intArrays[i]) {
+                answer = '?';
+            }
+        }
+
+        System.out.println(answer);
+
     }
 }
