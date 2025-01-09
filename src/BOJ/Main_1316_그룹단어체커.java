3987905dbc020f9c8a8c4ab7d9b78ad4facb7c2c
yuhyun <dlugus@naver.com>
2025-01-10 02:11:04 +0900

    1316 clear

--- src/BOJ/Main_1316_그룹단어체커.java
+++ src/BOJ/Main_1316_그룹단어체커.java
@@ -1,9 +1,41 @@
 package BOJ;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main_1316_그룹단어체커 {
     public static void main(String[] args) throws IOException {
-        //TODO 다시하기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        int cnt = 0;
+
+        for (int i = 0; i < t; i++) {
+            String word = br.readLine();
+
+            Set<Character> wordSet = new HashSet<>();
+            char prev = '\0';
+            boolean answer = true;
+
+            for (int j = 0; j < word.length(); j++) {
+                char now = word.charAt(j);
+
+                if (now != prev && wordSet.contains(now)) {
+                    answer = false;
+                }
+
+                prev = now;
+                wordSet.add(now);
+            }
+
+            if (answer) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
     }
 }
---
+++ src/BOJ/Main_1931_회의실배정.java
@@ -0,0 +1,4 @@
+package BOJ;
+
+public class Main_1931_회의실배정 {
+}
